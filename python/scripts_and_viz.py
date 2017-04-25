a16e90c49f4958c1b596270341ab9b5f32e27685
maheenrashid <mhnrashid@gmail.com>
2017-04-25 14:04:42 -0700

    corrected code for autothresh. messy scripts for visualizing and comparing

--- python/scripts_and_viz.py
+++ python/scripts_and_viz.py
@@ -622,7 +622,11 @@ def writeScriptSchemesFixThresh():
                 out_dir_meta_curr=os.path.join(out_dir_meta,str(epoch_start));
                 util.mkdir(out_dir_meta_curr);
                 if scheme=='mix':
-                    activationThreshMax=fixThresh*1000/epoch_start;
+                    if 1000%epoch_start==0:
+                        activationThreshMax=fixThresh*(1000/epoch_start-1);
+                    else:
+                        activationThreshMax=fixThresh*math.floor(1000/epoch_start);
+                    print activationThreshMax
                     command=writeBlurScript(path_to_th,out_dir_meta_curr,dir_files,fold_num,
                                             batchSize=batchSize,
                                             scheme=scheme,
@@ -658,16 +662,311 @@ def writeScriptSchemesFixThresh():
         util.writeFile(out_file_script_curr,commands);
 
 
+
+def writeScriptSchemesAutoThresh():
+
+    dir_exp_old='../experiments/khorrami_ck_rerun';
+    
+
+    experiment_name='ck_meanBlur_autoThresh_100_inc';
+    out_dir_meta_meta='../experiments/'+experiment_name;
+    util.mkdir(out_dir_meta_meta);
+    out_script='../scripts/train_'+experiment_name+'_mix';
+    num_scripts=2;
+    
+
+    util.mkdir(out_dir_meta_meta);
+    folds_range=[6];
+    epoch_starts=range(100,600,100);
+    startingActivation=0.05;
+    fixThresh=0.05;
+    activationThreshMax=0.2;
+    total_epochs=1000;
+    schemes=['ncl','mixcl']
+    epoch_starts=[[200,300,400,500],[500]];
+    # ,'mix'];
+    # schemes=['mix'];
+    path_to_th='train_khorrami_withBlur.th';
+
+    dir_files='../data/ck_96/train_test_files';
+    
+    # batchSizeTest=128;
+    batchSize=128;
+    ratioBlur=0.5;
+
+    commands=[];
+    for fold_num in folds_range:
+        data_file=os.path.join(dir_files,'train_'+str(fold_num)+'.txt');
+        num_lines=len(util.readLinesFromFile(data_file));
+        epoch_size=num_lines/batchSize;
+        for idx_scheme,scheme in enumerate(schemes):
+            out_dir_meta=os.path.join(out_dir_meta_meta,scheme);
+            util.mkdir(out_dir_meta);
+            for epoch_start in epoch_starts[idx_scheme]: 
+                out_dir_meta_curr=os.path.join(out_dir_meta,str(epoch_start));
+                util.mkdir(out_dir_meta_curr);
+                # if scheme=='mix':
+                #     if 1000%epoch_start==0:
+                #         activationThreshMax=fixThresh*(1000/epoch_start-1);
+                #     else:
+                #         activationThreshMax=fixThresh*math.floor(1000/epoch_start);
+                #     print activationThreshMax
+                #     command=writeBlurScript(path_to_th,out_dir_meta_curr,dir_files,fold_num,
+                #                             batchSize=batchSize,
+                #                             scheme=scheme,
+                #                             ratioBlur=ratioBlur,
+                #                             incrementDifficultyAfter=0,
+                #                             startingActivation=0,
+                #                             fixThresh=fixThresh,
+                #                             activationThreshMax=activationThreshMax,
+                #                             iterations=1000)
+                # else:
+                resume_model=os.path.join(dir_exp_old,str(fold_num),'intermediate','model_all_'+str(epoch_start*epoch_size)+'.dat');
+                assert(os.path.exists(resume_model));
+                if total_epochs%epoch_start==0:
+                    num_inc=total_epochs/epoch_start -1;
+                else:
+                    num_inc=math.floor(total_epochs/epoch_start);
+                startingActivation=activationThreshMax/num_inc;
+                print startingActivation,num_inc,startingActivation*num_inc
+
+                command=writeBlurScript(path_to_th,out_dir_meta_curr,dir_files,fold_num,
+                                        batchSize=batchSize,
+                                        model_file=resume_model,
+                                        scheme=scheme,
+                                        ratioBlur=ratioBlur,
+                                        incrementDifficultyAfter=epoch_start,
+                                        startingActivation=startingActivation,
+                                        fixThresh=0,
+                                        activationThreshMax=activationThreshMax,
+                                        iterations=total_epochs-epoch_start)
+                
+                commands.append(command);
+
+    len(commands)
+    commands=np.array(commands);
+    commands_split=np.array_split(commands,num_scripts);
+    for idx_commands,commands in enumerate(commands_split):
+        out_file_script_curr=out_script+'_'+str(idx_commands)+'.sh';
+        print idx_commands,out_file_script_curr,len(commands)
+        # print commands;
+        util.writeFile(out_file_script_curr,commands);
+
+
+def createComparativeHtml():
+    experiment_name='test_mean_blur';
+    out_dir_meta_meta=os.path.join(dir_server,'expression_project','experiments');
+    out_dir_meta=os.path.join(out_dir_meta_meta,'ck_meanBlur_fixThresh_100_inc')
+    out_dir_baseline=os.path.join(out_dir_meta_meta,'khorrami_ck_rerun');
+    dir_files='../data/ck_96/train_test_files';
+    dir_caption=['ncl','mixcl','mix','bl']
+    dir_comps=[os.path.join(out_dir_meta,dir_caption_curr,'200') for dir_caption_curr in dir_caption[:3]]+[out_dir_baseline];
+    range_folds=[6];
+    
+    expressions=[None]
+    right=True;
+
+    npy_file_pred='1_pred_labels.npy';
+    npy_file_gt='1_gt_labels.npy';
+    posts=['_org','_gb_gcam','_gb_gcam_pred','_hm','_hm_pred','_gb_gcam_org','_gb_gcam_org_pred']
+    
+    batchSizeTest=128;
+    for expression_curr in expressions:
+    
+        out_file_html=os.path.join(out_dir_meta,str(expression_curr)+'.html');
+        ims_all=[];
+        captions_all=[];
+        for fold_num in range_folds:    
+            data_path=os.path.join(dir_files,'train_'+str(fold_num)+'.txt');
+            gt_labels_file=os.path.join(dir_comps[0],str(fold_num),'test_images',npy_file_gt);
+            gt_labels=np.load(gt_labels_file);    
+            num_batches=int(math.ceil(len(gt_labels)/128.0))
+            im_pre=np.array([str(batch_num)+'_'+str(im_num) for batch_num in range(1,num_batches+1) for im_num in range(1,batchSizeTest+1)]);
+            im_pre=im_pre[:len(gt_labels)];
+            if expression_curr is not None:
+                bin_keep=gt_labels==expression_curr;
+            else:
+                bin_keep=gt_labels==gt_labels
+            gt_rel=gt_labels[bin_keep]
+            # print bin_keep.shape;
+            # print im_pre.shape
+            im_pre_rel=im_pre[bin_keep];
+
+            dir_ims=[os.path.join(dir_curr,str(fold_num),'test_images') for dir_curr in dir_comps];
+            pred_rels=[np.load(os.path.join(dir_curr,npy_file_pred))[bin_keep] for dir_curr in dir_ims];
+
+            for idx_im_pre_curr,im_pre_curr in enumerate(im_pre_rel):
+                for idx_epoch in range(len(dir_ims)):
+                    im_row=[util.getRelPath(os.path.join(dir_ims[idx_epoch],im_pre_curr+post_curr+'.jpg'),dir_server) for post_curr in posts];
+                    caption_pre='right' if pred_rels[idx_epoch][idx_im_pre_curr]==gt_rel[idx_im_pre_curr] else 'wrong'
+                    caption_row=[str(idx_im_pre_curr)+' '+caption_pre+' '+dir_caption[idx_epoch]+' '+str(int(gt_rel[idx_im_pre_curr]))+' '+str(int(pred_rels[idx_epoch][idx_im_pre_curr]))]*len(im_row);
+                    ims_all.append(im_row[:]);
+                    captions_all.append(caption_row[:])
+
+        visualize.writeHTML(out_file_html,ims_all,captions_all,100,100);
+        print out_file_html.replace(dir_server,click_str);
+
 def main():
-    writeScriptSchemesFixThresh()
-    # out_dir_meta='../experiments/khorrami_ck_rerun'
-    # num_folds=10;
-    # for fold_num in range(num_folds):
-    #     test_file=os.path.join(out_dir_meta,str(fold_num),'test_images','log_test.txt');
-    #     print test_file
-    #     lines=util.readLinesFromFile(test_file);
-    #     print fold_num
-    #     print lines[-1];
+
+    writeScriptSchemesAutoThresh()
+    return
+    experiment_name='scheme_200_inc';
+    out_dir_meta_meta=os.path.join(dir_server,'expression_project','experiments',experiment_name);
+    dir_files='../data/ck_96/train_test_files';
+    num_folds=1;
+    epoch_range=list(range(100,900,100));
+    epoch_range=[100,800];
+
+
+    expressions=[None]
+    range_folds=[6];
+    # range(1,9);
+    right=True;
+
+    npy_file_pred='1_pred_labels.npy';
+    npy_file_gt='1_gt_labels.npy';
+    schemes=['ncl','mixcl','mix','bl']
+    posts=['_org','_gb_gcam','_gb_gcam_pred','_hm','_hm_pred','_gb_gcam_org','_gb_gcam_org_pred']
+    # ,
+    #         '_gb_gcam_th','_gb_gcam_th_pred',
+    #         '_blur','_blur_pred']
+    
+    # num_folds=1;
+    # im_posts=[pre+exp_curr+'.jpg' for pre in pres for exp_curr in expressions]; 
+    
+
+    
+    batchSizeTest=64;
+    for scheme_curr in schemes:
+        out_dir_meta=os.path.join(out_dir_meta_meta,scheme_curr);
+        for expression_curr in expressions:
+        
+            out_file_html=os.path.join(out_dir_meta,str(expression_curr)+'.html');
+            ims_all=[];
+            captions_all=[];
+            for fold_num in range_folds:    
+                data_path=os.path.join(dir_files,'train_'+str(fold_num)+'.txt');
+                gt_labels_file=os.path.join(out_dir_meta,str(epoch_range[0]),str(fold_num),'test_images',npy_file_gt);
+                gt_labels=np.load(gt_labels_file);    
+                num_batches=int(math.ceil(len(gt_labels)/float(batchSizeTest)))
+                im_pre=np.array([str(batch_num)+'_'+str(im_num) for batch_num in range(1,num_batches+1) for im_num in range(1,batchSizeTest+1)]);
+                if expression_curr is not None:
+                    bin_keep=gt_labels==expression_curr;
+                else:
+                    bin_keep=gt_labels==gt_labels
+                gt_rel=gt_labels[bin_keep]
+                im_pre_rel=im_pre[bin_keep];
+
+                dir_ims=[os.path.join(out_dir_meta,str(epoch_num),str(fold_num),'test_images') for epoch_num in epoch_range];
+                pred_rels=[np.load(os.path.join(dir_curr,npy_file_pred))[bin_keep] for dir_curr in dir_ims];
+
+                for idx_im_pre_curr,im_pre_curr in enumerate(im_pre_rel):
+                    for idx_epoch in range(len(dir_ims)):
+                        im_row=[util.getRelPath(os.path.join(dir_ims[idx_epoch],im_pre_curr+post_curr+'.jpg'),dir_server) for post_curr in posts];
+                        caption_pre='right' if pred_rels[idx_epoch][idx_im_pre_curr]==gt_rel[idx_im_pre_curr] else 'wrong'
+                        caption_row=[str(idx_im_pre_curr)+' '+caption_pre+' '+str(epoch_range[idx_epoch])+' '+str(int(gt_rel[idx_im_pre_curr]))+' '+str(int(pred_rels[idx_epoch][idx_im_pre_curr]))]*len(im_row);
+                        ims_all.append(im_row[:]);
+                        captions_all.append(caption_row[:])
+
+            visualize.writeHTML(out_file_html,ims_all,captions_all,100,100);
+            print out_file_html.replace(dir_server,click_str);
+
+
+
+    return
+    experiment_name='scheme_200_inc';
+    # out_dir_meta='../experiments/'+experiment_name;
+    out_dir_meta='../experiments/'+experiment_name;
+    util.mkdir(out_dir_meta);
+    out_script='../scripts/test_'+experiment_name;
+    # +'.sh';
+    # path_to_th='train_khorrami_basic.th';
+    path_to_th='train_khorrami_withBlur.th';
+
+    dir_files='../data/ck_96/train_test_files';
+    # resume_dir_meta='../experiments/khorrami_basic_aug_fix_resume'
+
+    out_dir_meta_scheme=os.path.join(dir_server,'expression_project','experiments','ck_meanBlur_fixThresh_100_inc')
+    out_dir_baseline=os.path.join(dir_server,'expression_project','experiments','khorrami_ck_rerun');
+    schemes=['ncl','mixcl','mix','bl']
+    scheme_dirs=[os.path.join(out_dir_meta_scheme,dir_caption_curr,'200') for dir_caption_curr in schemes[:-1]]+[out_dir_baseline];
+    
+
+
+    range_folds=[6];
+
+    resume_dir_meta=None
+    noBias=False;
+    learningRate = 0.01;
+    batchSizeTest=128;
+    batchSize=128;
+    ratioBlur=0.0;
+
+    out_dir_model='../experiments/khorrami_ck_rerun';
+    
+    # print '{',
+    # out_script_curr=out_script+'_'+str(fold_num)+'.sh';
+
+    commands=[];
+    for idx_scheme,out_dir_scheme in enumerate(scheme_dirs):
+        for fold_num in range_folds:
+            for epoch_num in range(100,900,100):
+                out_dir_meta_curr=os.path.join(out_dir_meta,schemes[idx_scheme],str(epoch_num));
+                util.makedirs(out_dir_meta_curr);
+                out_dir_model_curr=os.path.join(out_dir_scheme,str(fold_num));
+                data_path=os.path.join(dir_files,'train_'+str(fold_num)+'.txt');
+                epoch_size=len(util.readLinesFromFile(data_path))/batchSize;
+                if schemes[idx_scheme]=='mix' or schemes[idx_scheme]=='ncl':
+                    inc_num=epoch_size*(epoch_num+200);
+                else:
+                    inc_num=epoch_size*epoch_num;
+                modelTest=os.path.join(out_dir_model_curr,'intermediate','model_all_'+str(inc_num)+'.dat');
+                print modelTest
+                assert os.path.exists(modelTest);
+                command=writeBlurScript(path_to_th,out_dir_meta_curr,dir_files,fold_num,modelTest=modelTest,batchSizeTest=64);
+                # print command;
+                commands.append(command);
+                # raw_input();
+    num_scripts=1;
+    # print out_script+'.sh'
+    # print len(commands);
+    # util.writeFile(out_script+'.sh',commands);
+
+    commands=np.array(commands);
+    commands_split=np.array_split(commands,num_scripts);
+    for idx_commands,commands in enumerate(commands_split):
+        out_file_script_curr=out_script+'_'+str(idx_commands)+'.sh';
+        print idx_commands
+        print out_file_script_curr
+        # print commands;
+        util.writeFile(out_file_script_curr,commands);
+
+
+    return
+
+    dir_meta='../experiments/ck_meanBlur_fixThresh_100_inc';
+    schemes=['mixcl','ncl'];
+    # schemes=['mix'];
+    inc_range=range(100,600,100);
+    num_fold=6;
+    for scheme in schemes:
+        for inc in inc_range:
+            file_curr=os.path.join(dir_meta,scheme,str(inc),str(num_fold),'test_images','log_test.txt');
+            lines=util.readLinesFromFile(file_curr);
+            print file_curr
+            print scheme,inc
+            print lines[-1];
+
+    return
+    # writeScriptSchemesFixThresh()
+    out_dir_meta='../experiments/khorrami_ck_rerun'
+    num_folds=10;
+    for fold_num in range(num_folds):
+        test_file=os.path.join(out_dir_meta,str(fold_num),'test_images','log_test.txt');
+        print test_file
+        lines=util.readLinesFromFile(test_file);
+        print fold_num
+        print lines[-1];
 
     # writeTFDSchemeScripts();
     # writeHTMLs_viz_inc();
